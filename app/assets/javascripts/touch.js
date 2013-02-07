0e55958abbab24d51840bafce12dfa096339b614
Frankie Roberto <frankie@frankieroberto.com>
2013-02-07 15:56:14 +0000

    Experimental touch support

--- app/assets/javascripts/touch.js
+++ app/assets/javascripts/touch.js
@@ -1,11 +1,33 @@
 
+touch_data = '';
 
-function touchMove(evnet) {
-  console.log(event);
+function touchMove(event) {
+}
+
+function touchStart(event) {
+  touch_data = 'TOUCHING';
+  getTouch();
+}
+
+function touchEnd(event) {
+  touch_data = '';
+  getTouch();
 }
 
 
 function getTouch() {
-  window.ontouchmove = touchMove;
+  var element = document.getElementById('touch');
+
+  if (touch_data === '') {
+    element.setAttribute('class', 'blank');
+    element.innerHTML = '(no touch)';
+  } else {
+    element.setAttribute('class', '');
+    element.innerHTML = touch_data;
+  }
+
 }
 
+
+document.addEventListener("DOMContentLoaded", getTouch, false);
+window.ontouchmove = touchMove;
