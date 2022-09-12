8277bfea6c51924f09d1cf24f7f0b26e3f1ec3d0
Frankie Roberto <frankie@frankieroberto.com>
2022-09-12 22:15:27 +0100

    Remove cached copy in _site

--- _site/javascripts/force_touch.js
+++
@@ -1,8 +0,0 @@
-export function forceChanged(event) {
-
-  var element = document.getElementById('force-touch');
-
-  var forceLevel = event["webkitForce"];
-
-  element.textContent = "Force level " + forceLevel;
-}
